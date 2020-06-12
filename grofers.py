788cb4e5a2e6c6bcac0d704d65e7256dcd4ea014
hiteshkhatana <47058253+hiteshkhatana@users.noreply.github.com>
2020-06-12 19:10:43 +0530

    Update grofers.py

--- grofers.py
+++ grofers.py
@@ -16,56 +16,57 @@ import os
 #PATH = "chromedriver.exe"
 
 
-def func(data):
-    ordered_list = []
+def func(item):
+	ordered_list = []
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    
-    driver = webdriver.Chrome(executable_path =os.environ.get("CHROMEDRIVER_PATH") , chrome_options=chrome_options)
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument("--no-sandbox")
 
-
-    driver.get(r"https://grofers.com/")
-    
-    #location = driver.find_element_by_class_name("Select-control")
-    #location.click()
-    #search = driver.find_element_by_css_selector("input[role='combobox']")
-    #search.send_keys("New delhi")
-    #search.send_keys(Keys.RETURN)
-    
-    time.sleep(3)
-    
-    for item in data["product"]:
-        search = driver.find_element_by_class_name("react-autosuggest__input")
-        driver.execute_script("arguments[0].click();", search)
-        search.send_keys(Keys.CONTROL + "a")
-        search.send_keys(Keys.DELETE)
-        search.send_keys(item)
-        search.send_keys(Keys.RETURN)
+	driver = webdriver.Chrome(executable_path =os.environ.get("CHROMEDRIVER_PATH") , chrome_options=chrome_options)
 
 
-        time.sleep(3)
+	driver.get(r"https://grofers.com/")
+
+	#location = driver.find_element_by_class_name("Select-control")
+	#location.click()
+	#search = driver.find_element_by_css_selector("input[role='combobox']")
+	#search.send_keys("New delhi")
+	#search.send_keys(Keys.RETURN)
+
+	time.sleep(3)
 
 
-        all_products = driver.find_elements_by_xpath("//div[@class='plp-product']")
-        for product in all_products:
-            product_name = product.find_element_by_class_name('plp-product__name--box').text
-            if product_name == item:
-                add = product.find_element_by_class_name('add-to-cart__add-btn')
-                driver.execute_script("arguments[0].click();", add)
-                ordered_list.append(item + " is ordered")
-                break
-
-    
+	search = driver.find_element_by_class_name("react-autosuggest__input")
+	driver.execute_script("arguments[0].click();", search)
+	search.send_keys(Keys.CONTROL + "a")
+	search.send_keys(Keys.DELETE)
+	search.send_keys(item)
+	search.send_keys(Keys.RETURN)
 
 
+	time.sleep(3)
 
-    time.sleep(2)
-    driver.quit()
-    return(ordered_list)
+
+	all_products = driver.find_elements_by_xpath("//div[@class='plp-product']")
+	for product in all_products:
+	    product_name = product.find_element_by_class_name('plp-product__name--box').text
+	    if product_name == item:
+		add = product.find_element_by_class_name('add-to-cart__add-btn')
+		driver.execute_script("arguments[0].click();", add)
+		ordered_list = item
+		return(ordered_list)
+		break
+
+
+
+
+
+	time.sleep(2)
+	driver.quit()
+	#return(ordered_list)
 
 
 
